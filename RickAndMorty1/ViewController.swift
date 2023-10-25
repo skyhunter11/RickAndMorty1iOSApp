8f80526ef6de1f0e7f7822f29426a3889339cdfa
Kane <ori.lowen@gmail.com>
2023-10-24 19:46:46 -1000

    Initial Commit

---
+++ RickAndMorty1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty1
+//
+//  Created by O Lowen on 10/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
